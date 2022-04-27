2c6ff9a0435041fc94921a98505a293c343fc351
Diego Calciolari Gordiano <diego.gordiano@acad.espm.br>
2022-04-27 14:41:17 -0300

    ta quase pronto

--- src/Processo.java
+++ src/Processo.java
@@ -17,18 +17,17 @@ public class Processo {
         Cliente c = null;
         switch(tipoPessoa){
             case Fisica:
-                String cpf = JOptionPane.showInputDialog("CPF: ");
-
+                String cpf = JOptionPane.showInputDialog("CPF: ");    
                 PessoaFisica pf = new PessoaFisica(nome, cpf);
                 c = pf;
                 break;
             case Juridica:
                 String cnpj = JOptionPane.showInputDialog("CNPJ: ");
-
                 PessoaJuridica pj = new PessoaJuridica(nome, cnpj);
                 c = pj;
                 break;
         }
+
 
         while(!aux.equals("s") && !aux.equals("n")){
             aux = JOptionPane.showInputDialog("O pagamento será à vista? [S/N]: ").toLowerCase();
@@ -36,24 +35,25 @@ public class Processo {
                 JOptionPane.showMessageDialog(null, "ERRO!! Informe um valor válido\nS: Pagamento à vista | N: Pagamento parcelado");
             }
         }
-        
+            
         if("s".equals(aux)){
             tipo = true;
         } else {
             tipo = false;        
         }
-
+    
         Reserva reserva = new Reserva(c, tipo);
         reserva.setPagamentoAVista(tipo);
-
+    
         reservas.add(reserva);
-        
+            
         if(reservas.size() > 6){
             JOptionPane.showMessageDialog(null, "Reservas esgotadas :( \nVocê está na lista de espera");
         } else{
             JOptionPane.showMessageDialog(null, "Reserva efetuada com sucesso");
         }
-    }
+    } 
+    
 
     private static TipoPessoa inputTipoCliente(){
 
@@ -73,36 +73,16 @@ public class Processo {
 
         if(reservas.size() > 0){
             String aux = JOptionPane.showInputDialog(null, "Informe seu CPF/CNPJ: ");
-            int achou=0;
+            int achou = verificar(aux);
 
-            for(int i = 0; i < reservas.size(); i++) {  
-                if(reservas.get(i).getCliente() instanceof PessoaFisica){
-                    Cliente c = reservas.get(i).getCliente();
-                    PessoaFisica pf = (PessoaFisica) (c);
-                    if(pf.getCpf().equals(aux)){
-                        achou++;
-                    }
-                }
-                if(reservas.get(i).getCliente() instanceof PessoaJuridica){
-                    Cliente c = reservas.get(i).getCliente();
-                    PessoaJuridica pj = (PessoaJuridica) (c);
-                    if(pj.getCnpj().equals(aux)){
-                        achou++;
-                    }
-                }
-            }
-
-            if(achou != 0){
-                JOptionPane.showMessageDialog(null, "Você possui uma reserva");
+            if(achou >= 0){
+                JOptionPane.showMessageDialog(null, "Você possui uma reserva"); 
             } else{
-                JOptionPane.showMessageDialog(null, "Você não possui uma reserva");    
+                JOptionPane.showMessageDialog(null, "Você não possui uma reserva");     
             }
-
         } else{
             JOptionPane.showMessageDialog(null, "ERRO!! Não existem reservas cadastradas!");     
-        }
-
-        
+        } 
     }
 
     public static void imprimirReservas(){
@@ -137,34 +117,45 @@ public class Processo {
 
         if(reservas.size() > 0){
             String aux = JOptionPane.showInputDialog(null, "Informe seu CPF/CNPJ: ");
+            int achou = verificar(aux);
+
+            if(achou >= 0){
+                reservas.remove(achou);
+                JOptionPane.showMessageDialog(null, "Reserva removida com sucesso!"); 
+            } else{
+                JOptionPane.showMessageDialog(null, "ERRO!! CPF/CNPJ não encontrado");     
+            }
+        } else{
+            JOptionPane.showMessageDialog(null, "ERRO!! Não existem reservas cadastradas!");     
+        } 
+    }
+
+
+    public static int verificar(String aux){
+
+        if(reservas.size() > 0){
 
             for(int i = 0; i < reservas.size(); i++) {  
                 if(reservas.get(i).getCliente() instanceof PessoaFisica){
                     Cliente c = reservas.get(i).getCliente();
                     PessoaFisica pf = (PessoaFisica) (c);
                     if(pf.getCpf().equals(aux)){
-                        reservas.remove(i);
-                        JOptionPane.showMessageDialog(null, "Reserva removida com sucesso!");
-                        break;  
-                    } else{
-                        JOptionPane.showMessageDialog(null, "ERRO!! CPF não encontrado!");  
-                        break;   
+                        return i;
                     }
-                } else if(reservas.get(i).getCliente() instanceof PessoaJuridica){
+                }
+                if(reservas.get(i).getCliente() instanceof PessoaJuridica){
                     Cliente c = reservas.get(i).getCliente();
                     PessoaJuridica pj = (PessoaJuridica) (c);
                     if(pj.getCnpj().equals(aux)){
-                        reservas.remove(i);
-                        JOptionPane.showMessageDialog(null, "Reserva removida com sucesso!");
-                        break;  
-                    } else{
-                        JOptionPane.showMessageDialog(null, "ERRO!! CPF não encontrado!"); 
-                        break;    
+                        return i;
                     }
                 }
             }
-        } else{
-            JOptionPane.showMessageDialog(null, "ERRO!! Não existem reservas cadastradas!");    
-        }
+
+        } 
+
+        return -1;
     }
+
+
 }
